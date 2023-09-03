595bbe5a4a8a9956a33923845dc1980ae1691aa4
Andrey Lutcenko <andrey.lutcenko90@mail.ru>
2023-09-03 18:18:48 +0500

    feat(ChatGPT): Подключен chatGPT + интеграцию notion

---
+++ src/chatgpt.js
@@ -0,0 +1,51 @@
+import OpenAI from 'openai';
+import config from 'config';
+
+const CHAT_GPT_MODEL = 'gpt-3.5-turbo';
+
+const ROLES = {
+  /** Отвечает за ответы чата */
+  ASSISTANT: 'assistant',
+  /** Системный ассистент для chatGPT */
+  SYSTEM: 'system',
+  /** Отвечат за отправку данных клиентом */
+  USER: 'user',
+};
+
+const openAI = new OpenAI({
+  apiKey: config.get('OPEN_AI_KEY'),
+});
+
+const getMessage = (mes) => `
+    Напиши на основе этих тезисов последовательную эмоциональную историю: ${mes}
+
+    Эти тезисы с описанием ключевых моментов дня.
+    Необходимо в итоге получить такую историю, что б я запомнил этот день и
+    смог в последствии рассказать её друзьям. Много текста не нужно, главное,
+    чтобы были эмоции, правильная последовательность с учётом контекста. 
+`;
+
+export async function chatGPT(message = '') {
+  const messages = [
+    {
+      role: ROLES.SYSTEM,
+      content:
+        'Ты опытный копирайтер, который пишет краткие эмоциональные статьи для социальных сетей.',
+    },
+    {
+      role: ROLES.USER,
+      content: getMessage(message),
+    },
+  ];
+
+  try {
+    const completion = await openAI.chat.completions.create({
+      messages,
+      model: CHAT_GPT_MODEL,
+    });
+
+    console.log(completion.choices[0].message);
+  } catch (error) {
+    console.error('Error while chat completion', error.message);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
-import { Telegraf } from "telegraf";
-import { message  } from "telegraf/filters"
-import config from "config";
+import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
+import config from 'config';
+import { chatGPT } from './chatgpt.js';
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'), {
-    /** Сколько по времени бот может ожидать ответа от сервера */
-    handlerTimeout: Infinity 
-})
+  /** Сколько по времени бот может ожидать ответа от сервера */
+  handlerTimeout: Infinity,
+});
 
-bot.command('start', context => {
-    context.reply('Добро пожаловать в бота. Отправьте текстовое сообщение с тезисами про историю.')
-})
+bot.command('start', (context) => {
+  context.reply(
+    'Добро пожаловать в бота. Отправьте текстовое сообщение с тезисами про историю.',
+  );
+});
 
-bot.on(message('text'), context => {
-    context.reply('test')
-})
+bot.on(message('text'), async (context) => {
+  await chatGPT(context.message.text);
+  context.reply('test');
+});
 
-bot.launch()
+bot.launch();
