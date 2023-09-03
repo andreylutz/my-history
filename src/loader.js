8d2ccffbb3d0b075faac4e443304acbffd63372a
Andrey Lutcenko <andrey.lutcenko90@mail.ru>
2023-09-03 19:42:38 +0500

    feat(Loader): Добавлен loader

---
+++ src/loader.js
@@ -0,0 +1,29 @@
+export class Loader {
+  icons = ['🕛', '🕐', '🕑', '🕒', '🕓', '🕔', '🕖', '🕗', '🕘', '🕙', '🕚'];
+  message = null;
+  interval = null;
+
+  constructor(context) {
+    this.context = context;
+  }
+
+  async show() {
+    let index = 0;
+    this.message = await this.context.reply(this.icons[index]);
+    this.interval = setInterval(() => {
+      index = index < this.icons.length - 1 ? index + 1 : 0;
+
+      this.context.telegram.editMessageText(
+        this.context.chat.id,
+        this.message.message_id,
+        null,
+        this.icons[index],
+      );
+    }, 500);
+  }
+
+  hide() {
+    clearInterval(this.interval);
+    this.context.telegram.deleteMessage(this.context.chat.id, this.message.message_id);
+  }
+}
